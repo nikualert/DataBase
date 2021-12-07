63247a8fe945c49796288103e753e7ddc2b75891
Студент <Student@43prof.ru>
2021-12-07 15:10:00 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+<h1>БД туристическое бюро </h1>
+
+<form action="welcome.php" method="post">
+маршрут: <input type="text" name="маршрут"><br>
+стоимость одного дня проживания : <input type="text" name="цена"><br>
+стоимость поездки:<input type="text" name="маршрут"><br>
+Клиент:<input type="text" name="маршрут"><br>
+заказ путёвок :<input type="text" name="маршрут"><br>
+маршрут категория:<input type="text" name="маршрут"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
